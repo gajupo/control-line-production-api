d59c244a60a07d6dbd86781519319fa77dd1dfae
Yao Gómez <yaoltzin@gmail.com>
2020-11-18 19:48:34 -0600

    Add the app/helpers/sequelize.js module, to simplify the initialization and connection with the database, and return a working sequelize instance

---
+++ app/helpers/sequelize.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const config = require('config');
+
+const {Sequelize, DataTypes, Model} = require('sequelize');
+const sequelize = new Sequelize(
+    config.get("database.name"),
+    config.get("database.user"),
+    config.get("database.password"), {
+    dialect: 'mssql',
+    host: config.get("database.host"),
+    port: config.get("database.port")
+});
+
+module.exports.sequelize = sequelize;
