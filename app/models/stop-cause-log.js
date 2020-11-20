f9f193a41593ec33ac67be838f02ca88be205577
Yao Gómez <yaoltzin@gmail.com>
2020-11-20 17:41:50 -0600

    Add the new model StopCauseLog, to manage the log of what caused the stop of the line.

---
+++ app/models/stop-cause-log.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const {DataTypes, Model} = require('sequelize');
+const {sequelize} = require("../helpers/sequelize");
+
+class StopCauseLog extends Model {};
+
+StopCauseLog.init({
+    createdDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'CreatedDate'
+    },
+    solutionDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        field: 'SolutionDate'
+    },
+    stopComments: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'StopComments'
+    },
+    status: {
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        field: 'Status'
+    },
+    stopCauseKeys: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'StopCauseKeys'
+    },
+    notificationSent: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'NotificationSent'
+    },
+    barcode: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: 'Barcode'
+    },
+}, {
+    tableName: 'StopCauseLogs',
+    timestamps: false,
+    sequelize
+});
