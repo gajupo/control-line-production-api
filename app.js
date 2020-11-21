04f245075cb05777fa7fecb6794af318b8674d0b
Yao Gómez <yaoltzin@gmail.com>
2020-11-20 19:51:49 -0600

    Add the route to handle the /stopcauselogs route and use the function getActiveStopCauseLogs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 const { getCustomerList } = require("./app/controllers/customer");
 const { getCurrentOrders } = require("./app/controllers/orders");
+const { getActiveStopCauseLogs } = require("./app/controllers/stop-cause-log");
 
 app.get('/customers', async (req, res, next) => {
     await getCustomerList(res, next);
@@ -13,6 +14,10 @@ app.get('/customers', async (req, res, next) => {
 
 app.get('/orders', async (req, res, next) => {
     await getCurrentOrders(res, next);
+});
+
+app.get('/stopcauselogs', async (req, res, next) => {
+    await getActiveStopCauseLogs(res, next);
 });
 
 app.listen(port, () => {
