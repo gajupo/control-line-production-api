2d1fa41080d093af02dec7f7ef8aa8210718a1c2
Yao Gómez <yaoltzin@gmail.com>
2020-11-23 20:58:03 -0600

    Add function successfulOperation to indicate a succesful operation (HTTP code 200)

--- app/controllers/core.js
+++ app/controllers/core.js
@@ -6,8 +6,18 @@ function notFoundError(message, res) {
         statusCode: 404,
         error: 'Not found',
         message: message
-    }
+    };
     res.status(404).send(JSON.stringify(response, null, 2));
 }
 
+function successfulOperation(message, res) {
+
+    const response = {
+        statusCode: 200,
+        message: message
+    };
+    res.status(200).send(JSON.stringify(response, null, 2));
+}
+
 module.exports.notFoundError = notFoundError;
+module.exports.successfulOperation = successfulOperation;
