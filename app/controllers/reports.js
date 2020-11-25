891b126309dec4a19287b3dccfef4e479bbb3613
Yao Gómez <yaoltzin@gmail.com>
2020-11-24 21:38:57 -0600

    If the Schema is not valid, send a bad request error.

--- app/controllers/reports.js
+++ app/controllers/reports.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const { Order, Material, OperatingStation, Shift, ReportParameterSchema } = require("../models");
+const { badRequestError } = require("./core");
 
 async function getPaginatedReportList(page, req, res, next) {
 
     try {
-        const {error, value} = ReportParameterSchema.validate(req.body);
+        const {error} = ReportParameterSchema.validate(req.body);
+
+        if (error) {
+            const errorList = error.details.map(e => e.message);
+            return badRequestError(`The schema is not valid`, res, errorList);
+        }
         res.send(JSON.stringify(req.body, null, 2));
     }
     catch(error) {
