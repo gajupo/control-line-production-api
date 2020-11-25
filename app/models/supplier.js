019cfc38d4401438187673b7a9b6d70fbe119c84
Yao Gómez <yaoltzin@gmail.com>
2020-11-24 18:44:59 -0600

    Add the supplier model, representing information about a material supplier.

---
+++ app/models/supplier.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {DataTypes, Model} = require('sequelize');
+const {sequelize} = require("../helpers/sequelize");
+
+module.exports = sequelize.define('Supplier', {
+    supplierName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'SupplierName'
+    },
+    supplierNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'SupplierNumber'
+    },
+    supplierEmail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'SupplierEmail'
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'Status'
+    }
+}, {
+    tableName: 'Supliers',
+    timestamps: false,
+    sequelize
+});
