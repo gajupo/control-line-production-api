e293c9f3047e258dadaddb43495d3b2425efdc4e
Yao Gómez <yaoltzin@gmail.com>
2020-11-25 19:53:43 -0600

    Add a temporal fix to an issue with Dates and MSSQL

--- app/helpers/sequelize.js
+++ app/helpers/sequelize.js
@@ -3,6 +3,12 @@
 const config = require('config');
 const {Sequelize} = require('sequelize');
 
+// TODO: Check regularly for an official fix to this issue
+// https://github.com/sequelize/sequelize/issues/7879
+Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
+    return this._applyTimezone(date, options).format('YYYY-MM-DD HH:mm:ss.SSS');
+}
+
 const sequelize = new Sequelize(
     config.get("database.name"),
     config.get("database.user"),
