f1fbb34c0a171471b83bafc732aca8eb71ca33b5
Yao Gómez <yaoltzin@gmail.com>
2020-11-30 18:14:09 -0600

    Retrieve all the neccesary models and fields required for the orders.

--- app/controllers/orders.js
+++ app/controllers/orders.js
@@ -2,23 +2,33 @@
 
 const { logError } = require('../helpers/logger');
 const { internalServerError } = require("./core");
-const { Order, OperatingStation, ProductionLine } = require("../models");
+const { Order, Material, Customer, ProductionLine, Shift } = require("../models");
 
 async function getCurrentOrders(res, next) {
 
     try {
         const orders = await Order.findAll({
+            attributes: ['id', 'orderIdentifier', 'materialScanned', 'orderGoal', 'isIncomplete'],
             include: [{
-                model: OperatingStation,
+                model: Material,
+                attributes: ['id', 'pasPN'],
                 include: [{
-                    model: ProductionLine
+                    model: Customer,
+                    attributes: ['id', 'customerName']
                 }]
+            }, {
+                model: ProductionLine,
+                attributes: ['id', 'lineName']
+            }, {
+                model: Shift,
+                attributes: ['id', 'shiftDescription']
             }]
         });
         res.send(JSON.stringify(orders, null, 2));
     }
     catch(error) {
         logError("Error in getCurrentOrders", error);
+        console.log(error);
         return internalServerError(`Internal server error`, res);
     }
 }
