7f2bd5d1eec6da3c69a8649b2a290763697338c8
Yao Gómez <yaoltzin@gmail.com>
2021-07-14 20:10:21 -0500

    Add overloaded method getProductionLines, to retrieve the production lines for the given customer.

--- app/controllers/production-lines.js
+++ app/controllers/production-lines.js
@@ -5,7 +5,7 @@ const { internalServerError, badRequestError } = require("./core");
 const { Sequelize, Op } = require('sequelize');
 const { parseISO } = require('date-fns');
 const { ProductionLine, OperatingStation, validateProductionLineParameters, Customer,
-    ValidationResult, Order, Material, Shift, StopCauseLog } = require('../models');
+    ValidationResult, Order, Material, Shift, StopCauseLog, validateModelId } = require('../models');
 
 async function getProductionLines(res) {
     try {
@@ -15,6 +15,29 @@ async function getProductionLines(res) {
                 attributes: ['id', 'stationIdentifier']
             }],
             attributes: ['id', 'lineName']
+        });
+        res.json(productionlines);
+    }
+    catch (error) {
+        logError("Error in getProductionLines", error);
+        return internalServerError(`Internal server error`, res);
+    }
+}
+
+async function getProductionLines(req, res) {
+    try {
+        const customer = validateModelId(req.params.customerId);
+        if (!customer.isValid) {
+            return badRequestError("Invalid parameter", res, customer.errorList);
+        }
+        const productionlines = await ProductionLine.findAll({
+            attributes: ['id', 'lineName'],
+            include: [{
+                model: Customer,
+                required: true,
+                attributes: [],
+                where: { id: customer.id }
+            }]
         });
         res.json(productionlines);
     }
