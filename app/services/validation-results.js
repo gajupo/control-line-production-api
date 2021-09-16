b73606bdf5d5e51096aae4c9ac7a5a2496bb8093
Gaudencio Jurado <gjurado@virtualidevs.com>
2021-09-15 22:41:01 -0500

    added a function to get validation result per hour in a better way

--- app/services/validation-results.js
+++ app/services/validation-results.js
@@ -1,7 +1,7 @@
 const { Sequelize, Op, QueryTypes } = require('sequelize');
+const { utcToZonedTime, format } = require('date-fns-tz');
 const { sequelize } = require("../helpers/sequelize");
 const models = require("../models");
-
 async function getProductionComplianceImpl(line, today) {
     try {
         const validationResults = await models.ValidationResult.findAll({
@@ -36,5 +36,42 @@ async function getProductionComplianceImpl(line, today) {
         throw new Error(error);
     }
 }
+async function getValidationResultsPerHourImpl(params) {
+    try {
+        const dateValue = utcToZonedTime(params.date,'America/Mexico_City');
+        const pattern = 'yyyy-MM-dd HH:mm:ss';
+        const validations = await sequelize.query(
+            `select sum(result.validationResults) as [validationResults], sum(result.goalPerOrder) as [productionRates], result.scanHour as [hours] 
+            from (
+                select 
+                count(ValidationResults.id) as validationResults,
+                ( ( (DATEDIFF(SECOND,min(ValidationResults.ScanDate),max(ValidationResults.ScanDate)) / 60) * Materials.ProductionRate ) / 60) as goalPerOrder,
+                DATEPART(HOUR, ValidationResults.ScanDate) as scanHour,
+                --min(ValidationResults.ScanDate) as mindate,
+                --max(ValidationResults.ScanDate) as maxdate,
+                --DATEDIFF(SECOND,min(ValidationResults.ScanDate),max(ValidationResults.ScanDate)) as usedSeconds, 
+                ValidationResults.OrderIdentifier
+                --Materials.ProductionRate
+                from ValidationResults
+                inner join Materials on Materials.ID = ValidationResults.MaterialId
+                inner join Orders on Orders.Id = ValidationResults.OrderId and Orders.ProductionLineId = $productionLineId and Orders.ShiftId = $shiftId
+                where 
+                CONVERT(date, ValidationResults.ScanDate) = $dateValue and ValidationResults.CustomerId = $customerId 
+                GROUP BY DATEPART(HOUR, ValidationResults.ScanDate), ValidationResults.OrderIdentifier,Materials.ProductionRate
+            ) as result
+            group by result.scanHour`,
+            {
+            bind: { dateValue: format(dateValue, pattern), productionLineId: params.productionLineId, shiftId: params.shiftId, customerId: params.customerId},
+            raw: true,
+            type: QueryTypes.SELECT
+            }
+        );
+    return validations;
+    } catch (error) {
+        throw new Error(error);
+    }
+    
+}
 
 module.exports.getProductionComplianceImpl = getProductionComplianceImpl;
+module.exports.getValidationResultsPerHourImpl = getValidationResultsPerHourImpl;
