3f30cc40b79fbc1c478e8ff8178fc2ed9ade0bde
Gaudencio Jurado <gjurado@virtualidevs.com>
2021-10-08 17:11:50 -0500

    added functions to obtain the differecen in minutes of the shift

--- app/helpers/lib.js
+++ app/helpers/lib.js
@@ -20,6 +20,34 @@ function getShiftHour(shiftStringTime) {
     return hour;
 
 }
+function getShiftMinutes(shiftStringTime) {
+    let re = /^([0-1]?\d|2[0-3])(?::([0-5]?\d))?(?::([0-5]?\d))?$/;
+    let timeArray = shiftStringTime.toString().match(re);
+    let minutes = 0;
+    if(timeArray){
+        const today = utcToZonedTime(new Date(), "America/Mexico_City");
+        const isoDate = datefns.formatISO(today, { representation: 'date' }) + ' ' + shiftStringTime;
+        minutes = new Date(isoDate).getMinutes();
+    }
+    console.log(minutes);
+    return minutes;
+
+}
+function getShiftDifferenceInMinutes(shiftStrStartTime, shiftStrEndTime) {
+    let re = /^([0-1]?\d|2[0-3])(?::([0-5]?\d))?(?::([0-5]?\d))?$/;
+    let startTimeArray = shiftStrStartTime.toString().match(re);
+    let endTimeArray = shiftStrEndTime.toString().match(re);
+    let minutes = 0;
+    if(startTimeArray && endTimeArray){
+        const today = utcToZonedTime(new Date(), "America/Mexico_City");
+        const isoStartDate = datefns.formatISO(today, { representation: 'date' }) + ' ' + shiftStrStartTime;
+        const isoEndDate = datefns.formatISO(today, { representation: 'date' }) + ' ' + shiftStrEndTime;
+        minutes = datefns.differenceInMinutes(datefns.parseISO(isoEndDate),datefns.parseISO(isoStartDate),{roundingMethod:'ceil'});
+    }
+    console.log(minutes);
+    return minutes;
+
+}
 function Round(value) {
     const neat = +(Math.abs(value).toPrecision(15));
     const rounded = Math.round(neat * 100) / 100;
@@ -29,4 +57,6 @@ function Round(value) {
 module.exports.isObject = isObject;
 module.exports.isArray = isArray;
 module.exports.getShiftHour = getShiftHour;
+module.exports.getShiftMinutes = getShiftMinutes;
 module.exports.Round = Round;
+module.exports.getShiftDifferenceInMinutes = getShiftDifferenceInMinutes;
