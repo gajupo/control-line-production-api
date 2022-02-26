3558938305b5a45b25d86990d9f8df411f7beefb
Cesar Suarez <cesar.suarez@virtualidevs.com>
2022-02-25 18:30:05 -0600

    changed function to return in base rolId and UserCustomer only productLines assigned

--- app/controllers/customer.js
+++ app/controllers/customer.js
@@ -1,14 +1,26 @@
 const fsp = require('fs').promises;
 const { logError, logger } = require('../helpers/logger');
-const { Customer } = require('../models');
+const { Customer, UserCustomer, ProductionLine } = require('../models');
 const { internalServerError } = require('./core');
 
 // eslint-disable-next-line no-unused-vars
-async function getCustomerList(res, next) {
+async function getCustomerList(req, res, next) {
   try {
     const customers = await Customer.findAll({
-      attributes: ['id', 'customerNumber', 'customerName'],
+      attributes: [['Id','id'],['CustomerName', 'customerName']],
+      include:[{
+        model: UserCustomer,
+        attributes:[],
+        where: { userId: req.user.userId },
+      },{
+        as: 'lineas',
+        model: ProductionLine,
+        attributes: ['id', ['lineName', 'LineName']],
+      }]
     });
+    /*const customers = await Customer.findAll({
+      attributes: ['id', 'customerNumber', 'customerName'],
+    });*/
     logger.debug('Customer found ', customers);
     return res.json(customers);
   } catch (error) {
