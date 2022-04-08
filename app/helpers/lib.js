7dd1b3b1ee15ddd2d5ec98085f252deeb3c54c6c
Cesar Suarez <cesar.suarez@virtualidevs.com>
2022-04-07 20:13:25 -0500

    functions for formatt status boolean to string

--- app/helpers/lib.js
+++ app/helpers/lib.js
@@ -10,6 +10,16 @@ function Round(value) {
   const rounded = Math.round(neat * 100) / 100;
   return rounded * Math.sign(value);
 }
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+function getStringByStatus(value) {
+  return value ? 'Activo' : 'Inactivo';
+}
+
 module.exports.isObject = isObject;
 module.exports.isArray = isArray;
 module.exports.Round = Round;
+module.exports.getKeyByValue = getKeyByValue;
+module.exports.getStringByStatus = getStringByStatus;
