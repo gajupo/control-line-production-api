b3a4798882071714738b5485cead725a389c8755
Cesar Suarez <cesar.suarez@virtualidevs.com>
2022-04-07 20:09:50 -0500

    added user controller and routes

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,6 +1,9 @@
+const { Op } = require('sequelize');
 const { logError, logger } = require('../helpers/logger');
-const { UserSectionPermissions, ApplicationSections } = require('../models');
+const { UserSectionPermissions, ApplicationSections, User } = require('../models');
 const { internalServerError } = require('./core');
+// const { Role } = require('../helpers/role');
+// const { getKeyByValue, getStringByStatus } = require('../helpers/lib');
 
 async function getSectionPermissionsList(parameters, res) {
   try {
@@ -29,5 +32,105 @@ async function getSectionPermissionsList(parameters, res) {
     return internalServerError('Internal server error', res);
   }
 }
+async function getUsersList(parameters, res) {
+  try {
+    const users = await User.findAll({
+      raw: true,
+      nest: true,
+      attributes: ['id', ['userName', 'username'], 'name', ['firstName', 'firstname'], ['emailAddress', 'email'], 'status', ['userTypeId', 'usertype'], 'lockcode'],
+      where: { id: { [Op.ne]: parameters.UserId } },
+    });/* .then((usersList) => {
+      usersList.map((user) => {
+        // eslint-disable-next-line no-param-reassign
+        user.usertype = getKeyByValue(Role, user.usertype);
+        return user;
+      });
+      return usersList;
+    }); */
+    logger.debug('getUsersList consumed by ', parameters);
+    return res.json(users);
+  } catch (error) {
+    logError('Error in getUsersList', error);
+    return internalServerError('Internal server error', res);
+  }
+}
+async function getUser(parameters, res) {
+  try {
+    const userData = await User.findAll({
+      raw: true,
+      nest: true,
+      attributes: ['id', ['userName', 'username'], 'name', ['firstName', 'firstname'], ['emailAddress', 'email'], 'status', ['userTypeId', 'usertype'], 'lockcode'],
+      where: { id: { [Op.eq]: parameters.UserId } },
+    });
+    logger.debug('getUser consumed by ', parameters);
+    return res.json(userData);
+  } catch (error) {
+    logger.debug('Error in getUser ', error);
+    logError('Error in getUser', error);
+    return internalServerError('Internal server error', res);
+  }
+}
+
+async function setBulkUpdate(parameters, res) {
+  try {
+    const result = await User.update({
+      status: parameters.status,
+    }, {
+      where: {
+        id: {
+          [Op.in]: parameters.users.map((item) => item),
+        },
+      },
+    });
+    logger.debug('setBulkUpdate consumed by ', parameters);
+    return res.json(result);
+  } catch (error) {
+    logger.debug('Error in setBulkUpdate ', error);
+    logError('Error in setBulkUpdate', error);
+    return internalServerError('Internal server error', res);
+  }
+}
+
+async function setProfileUpdateStatus(parameters, res) {
+  try {
+    const user = await User.update({
+      status: parameters.user.status,
+    }, {
+      where: { id: parameters.user.id },
+      returning: true,
+      plain: true,
+    });
+    logger.debug('setProfileUpdateStatus consumed by ', parameters);
+    return res.json(user);
+  } catch (error) {
+    logger.debug('Error in setProfileUpdateStatus ', error);
+    logError('Error in setProfileUpdateStatus', error);
+    return internalServerError('Internal server error', res);
+  }
+}
+async function setProfileUpdateGeneral(parameters, res) {
+  try {
+    const user = await User.update({
+      status: parameters.user.status,
+      lockCode: parameters.user.lockcode,
+      userTypeId: parameters.user.usertype,
+    }, {
+      where: { id: parameters.user.id },
+      returning: true,
+      plain: true,
+    });
+    logger.debug('setProfileUpdateStatus consumed by ', parameters);
+    return res.json(user);
+  } catch (error) {
+    logger.debug('Error in setProfileUpdateStatus ', error);
+    logError('Error in setProfileUpdateStatus', error);
+    return internalServerError('Internal server error', res);
+  }
+}
 
 module.exports.getSectionPermissionsList = getSectionPermissionsList;
+module.exports.getUsersList = getUsersList;
+module.exports.getUser = getUser;
+module.exports.setBulkUpdate = setBulkUpdate;
+module.exports.setProfileUpdateStatus = setProfileUpdateStatus;
+module.exports.setProfileUpdateGeneral = setProfileUpdateGeneral;
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const {
   getSectionPermissionsList,
+  getUsersList,
+  getUser,
+  setBulkUpdate,
+  setProfileUpdateStatus,
+  setProfileUpdateGeneral,
 } = require('../controllers/user');
 const { auth } = require('../middleware/auth');
 const authorize = require('../middleware/authorize');
@@ -18,5 +23,61 @@ router.get('/sectionpermissions', [auth, authorize([Role.Administrador, Role.Sup
     Name: req.user.name,
   };
   await getSectionPermissionsList(parameters, res);
+});
+/**
+ * GET /api/user/users
+ */
+router.get('/users', [auth, authorize([Role.Administrador])], async (req, res) => {
+  const parameters = {
+    UserId: req.user.userId,
+    RolId: req.user.rolId,
+    UserName: req.user.userName,
+    Name: req.user.name,
+  };
+  await getUsersList(parameters, res);
+});
+/**
+ * GET /api/user/user
+ */
+router.get('/user', [auth, authorize([Role.Administrador, Role.Supervisor, Role.Operador])], async (req, res) => {
+  const parameters = {
+    UserId: req.user.userId,
+    RolId: req.user.rolId,
+    UserName: req.user.userName,
+    Name: req.user.name,
+  };
+  await getUser(parameters, res);
+});
+/**
+ * POST /api/user/bulkupdate
+ */
+router.post('/bulkupdate', [auth, authorize([Role.Administrador])], async (req, res) => {
+  const parameters = {
+    UserId: req.user.userId,
+    RolId: req.user.rolId,
+    UserName: req.user.userName,
+    Name: req.user.name,
+    users: req.body.users,
+    status: req.body.type === 2,
+  };
+  await setBulkUpdate(parameters, res);
+});
+/**
+ * POST /api/user/profileupdate
+ */
+router.post('/profileupdate', [auth, authorize([Role.Administrador])], async (req, res) => {
+  const parameters = {
+    UserId: req.user.userId,
+    RolId: req.user.rolId,
+    UserName: req.user.userName,
+    Name: req.user.name,
+    user: req.body.user,
+  };
+  if (req.body.type === 1) {
+    await setProfileUpdateStatus(parameters, res);
+  }
+  if (req.body.type === 2) {
+    await setProfileUpdateGeneral(parameters, res);
+  }
 });
 module.exports = router;
