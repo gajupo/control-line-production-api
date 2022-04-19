74346241c8732b502535a4e26d042cca357e3c5b
Cesar Suarez <cesar.suarez@virtualidevs.com>
2022-04-19 12:44:57 -0500

    added functions of added for user types

--- app/controllers/user-types.js
+++ app/controllers/user-types.js
@@ -1,7 +1,8 @@
 const { Op } = require('sequelize');
-const { logError, logger } = require('../helpers/logger');
-const { UserType } = require('../models');
-const { internalServerError } = require('./core');
+const { returnError } = require('./core');
+const { sequelize } = require('../helpers/sequelize');
+const { logError, logMessage } = require('../helpers/logger');
+const { UserType, UserSectionPermissions } = require('../models');
 
 async function getUserTypesList(parameters, res) {
   try {
@@ -10,11 +11,12 @@ async function getUserTypesList(parameters, res) {
       nest: true,
       attributes: ['id', 'name', 'description', 'status'],
     });
-    logger.debug('getUserTypesList consumed by ', parameters);
+    logMessage(`UserTypes Controller getUserTypesList consumed by ${JSON.stringify(parameters)}`);
     return res.json(usertypes);
   } catch (error) {
-    logError('Error in getUserTypesList', error);
-    return internalServerError('Internal server error', res);
+    logError('Error in Controller UserTypes getUserTypesList details ', error, parameters);
+    logError('Error in Controller UserTypes getUserTypesList compressed', error.response ? error.response.data : error.message, parameters);
+    return returnError(error, res);
   }
 }
 
@@ -29,13 +31,103 @@ async function setBulkUpdate(parameters, res) {
         },
       },
     });
-    logger.debug('setBulkUpdate consumed by ', parameters);
+    logMessage(`UserTypes Controller setBulkUpdate consumed by ${JSON.stringify(parameters)}`);
     return res.json(result);
   } catch (error) {
-    logError('Error in setBulkUpdate', error);
-    return internalServerError('Internal server error', res);
+    logError('Error in Controller UserTypes setBulkUpdate details ', error, parameters);
+    logError('Error in Controller UserTypes setBulkUpdate compressed', error.response ? error.response.data : error.message, parameters);
+    return returnError(error, res);
+  }
+}
+
+async function setRolUpdateStatus(parameters, res) {
+  try {
+    const usertype = await UserType.update({
+      status: parameters.usertype.status,
+    }, {
+      where: { id: parameters.usertype.id },
+      returning: true,
+      plain: true,
+    });
+    logMessage(`UserTypes Controller setRolUpdateStatus consumed by ${JSON.stringify(parameters)}`);
+    return res.json(usertype);
+  } catch (error) {
+    logError('Error in Controller UserTypes setRolUpdateStatus details ', error, parameters);
+    logError('Error in Controller UserTypes setRolUpdateStatus compressed', error.response ? error.response.data : error.message, parameters);
+    return returnError(error, res);
+  }
+}
+async function setRolUpdateGeneral(parameters, res) {
+  try {
+    const usertype = await UserType.update({
+      status: parameters.usertype.status,
+      name: parameters.usertype.name,
+      description: parameters.usertype.description,
+    }, {
+      where: { id: parameters.usertype.id },
+      returning: true,
+      plain: true,
+    });
+    logMessage(`UserTypes Controller setRolUpdateGeneral consumed by ${JSON.stringify(parameters)}`);
+    return res.json(usertype);
+  } catch (error) {
+    logError('Error in Controller UserTypes setRolUpdateGeneral details ', error, parameters);
+    logError('Error in Controller UserTypes setRolUpdateGeneral compressed', error.response ? error.response.data : error.message, parameters);
+    return returnError(error, res);
+  }
+}
+async function setSectionPermissionsbyUserType(parameters, res) {
+  const t = await sequelize.transaction();
+  try {
+    const Promises = [];
+    if (parameters.sectionpermissionsDelete.length > 0) {
+      for (let i = 0; i < parameters.sectionpermissionsDelete.length; i++) {
+        const promiseDelete = UserSectionPermissions.destroy({
+          where: {
+            [Op.and]: [
+              { applicationSectionId: parameters.sectionpermissionsDelete[i] },
+              { userTypeId: parameters.usertype.id },
+            ],
+          },
+          transaction: t,
+        });
+        Promises.push(promiseDelete);
+      }
+    }
+    if (parameters.sectionpermissionsAdd.length > 0) {
+      for (let i = 0; i < parameters.sectionpermissionsAdd.length; i++) {
+        const promiseAdd = UserSectionPermissions.findOrCreate({
+          where: {
+            applicationSectionId: parameters.sectionpermissionsAdd[i],
+            userTypeId: parameters.usertype.id,
+            active: true,
+          },
+          defaults: {
+            applicationSectionId: parameters.sectionpermissionsAdd[i],
+            userTypeId: parameters.usertype.id,
+            active: true,
+          },
+          transaction: t,
+        });
+        Promises.push(promiseAdd);
+      }
+    }
+    Promise.all(Promises).then(async (result) => {
+      await t.commit();
+      logMessage(`setSectionPermissionsbyUserType ${JSON.stringify(result)}`);
+    });
+    logMessage(`UserTypes Controller setSectionPermissionsbyUserType consumed by ${JSON.stringify(parameters)}`);
+    return res.json(true);
+  } catch (error) {
+    t.rollback();
+    logError('Error in Controller UserTypes setSectionPermissionsbyUserType details ', error, parameters);
+    logError('Error in Controller UserTypes setSectionPermissionsbyUserType compressed', error.response ? error.response.data : error.message, parameters);
+    return returnError(error, res);
   }
 }
 
 module.exports.getUserTypesList = getUserTypesList;
 module.exports.setBulkUpdate = setBulkUpdate;
+module.exports.setRolUpdateStatus = setRolUpdateStatus;
+module.exports.setRolUpdateGeneral = setRolUpdateGeneral;
+module.exports.setSectionPermissionsbyUserType = setSectionPermissionsbyUserType;
--- app/routes/user-types.js
+++ app/routes/user-types.js
@@ -2,6 +2,9 @@ const express = require('express');
 const {
   getUserTypesList,
   setBulkUpdate,
+  setRolUpdateStatus,
+  setRolUpdateGeneral,
+  setSectionPermissionsbyUserType,
 } = require('../controllers/user-types');
 const { auth } = require('../middleware/auth');
 const authorize = require('../middleware/authorize');
@@ -35,5 +38,36 @@ router.post('/bulkupdate', [auth, authorize([Role.Administrador])], async (req, 
     type: req.body.type === 2,
   };
   await setBulkUpdate(parameters, res);
+});
+/**
+ * POST /api/usertype/rolupdate
+ */
+router.post('/rolupdate', [auth, authorize([Role.Administrador])], async (req, res) => {
+  const parameters = {
+    UserId: req.user.userId,
+    RolId: req.user.rolId,
+    UserName: req.user.userName,
+    Name: req.user.name,
+    usertype: req.body.usertype,
+  };
+  if (req.body.type === 1) {
+    await setRolUpdateStatus(parameters, res);
+  }
+  if (req.body.type === 2) {
+    await setRolUpdateGeneral(parameters, res);
+  }
+});
+// setSectionPermissionsbyUserType
+router.post('/sectionpermissionsupdate', [auth, authorize([Role.Administrador])], async (req, res) => {
+  const parameters = {
+    UserId: req.user.userId,
+    RolId: req.user.rolId,
+    UserName: req.user.userName,
+    Name: req.user.name,
+    usertype: req.body.usertype,
+    sectionpermissionsDelete: req.body.sectionpermissionsDelete,
+    sectionpermissionsAdd: req.body.sectionpermissionsAdd,
+  };
+  await setSectionPermissionsbyUserType(parameters, res);
 });
 module.exports = router;
